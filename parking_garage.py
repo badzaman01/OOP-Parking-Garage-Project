25c64c9edbfcfc9f5d7b54124097e36202c0669f
badza <badzaman01@gmail.com>
2022-08-21 19:54:15 -0400

    final commit

--- parking_garage.py
+++ parking_garage.py
@@ -7,56 +7,68 @@ time_in = now.strftime("%H:%M:%S")
 # print("Current Time: ", time_in)
 
 class ParkingGarage:
-    def __init__(self, parking_spaces, available_tickets):
-        self.parking_spaces = parking_spaces
-        self.available_tickets = available_tickets
+    def __init__(self):
+        self.parking_spaces = [100]
+        self.available_tickets = [100]
         self.current_ticket = {}
     
-    def take_ticket(self):
-        now = datetime.now()
-        time_in = now.strftime("%H:%M:%S")
-        print(f"You arrived at {time_in}.")
-        if self.available_tickets > 0:
-            global new_ticket
-            new_ticket = int(input("How many tickets would you like to purchase today? "))
-            ticket_num = randint(1, 101)
-            self.current_ticket[ticket_num] = False
-            print(f"Here is your ticket! Enjoy your stay! #{ticket_num}")
-            self.available_tickets -= new_ticket
-            self.parking_spaces -= new_ticket
-
     def show_available_spaces(self):
-        print(f"There are {self.parking_spaces} parking spaces available")
+        print(f"There are {self.parking_spaces[0]} parking spaces available")
+    
+    def check_if_paid(self):
+        ticket_input = int(input("What is the number of your ticket?"))
+        for x in self.current_ticket:
+            if x == ticket_input:
+                if self.current_ticket[x] == True:
+                    print("paid")
+                elif self.current_ticket[x] == False:
+                    print("not paid")
+                else:
+                    print("This ticket number isn't here...")
 
-
-    def pay_for_parking(self):
-        pay = int(input("Please enter the number found on your ticket: "))
-        if pay not in self.current_ticket.keys():
-            print(f"Ticket #{pay} not in system. Please try again. Bozo." )
-        else:
-            total_amount = randint(5, 10)
-            pay_now = input(f"Your total amount is ${total_amount}. Would you like to pay this now? Y/N ")
-            if pay_now.lower() in ("y", "yes"):
-                self.current_ticket[pay] = True
-                print("Your payment is confirmed.")
-            elif pay_now.lower() in ('n', 'no'):
-                print("Good luck, you're stuck forever, mwahahaha!")
-            else:
-                print("Please pick a valid option.")
-                
     def leave_garage(self):
         pay = int(input("You are now leaving the garage. Please enter the number found on your ticket to confirm payment: "))
         if pay not in self.current_ticket.keys():
             print(f"Ticket #{pay} not in system. Please try again. Bozo." )
         elif self.current_ticket[pay] == True:
-            print("You have paid your ticket! Thank You! Have a nice day! Or don't. I don't care.")
+            print(f"You have paid your ticket at! It is now {time_now}, and you are leaving the garage. Thank You! Have a nice day! Or don't. I don't care.")
         else:
             print("You haven't paid for parking yet. Don't steal.")
             ParkingGarage.pay_for_parking(self)
-        self.available_tickets += new_ticket 
-        self.parking_spaces += new_ticket
+        self.parking_spaces[0] += new_ticket
+        self.available_tickets[0] += new_ticket
+
+class GarageTransactions(ParkingGarage):
+    def take_ticket(self):
+        now = datetime.now()
+        global time_now
+        time_now = now.strftime("%H:%M:%S")
+        print(f"You arrived at {time_now}.")
+        if self.available_tickets[0] > 0:
+            global new_ticket
+            new_ticket = int(input("How many tickets would you like to purchase today? "))
+            ticket_num = randint(1, 101)
+            self.current_ticket[ticket_num] = False
+            print(f"Here is your ticket! Enjoy your stay! #{ticket_num}")
+            self.parking_spaces[0] -= new_ticket
+            self.available_tickets[0] -= new_ticket
+
+    def pay_for_parking(self):
+        pay = int(input("Please enter the number found on your ticket: "))
+        if pay not in self.current_ticket.keys():
+            print(f"Ticket #{pay} not in system. Please try again. Bozo.")
+        else:
+            total_amount = randint(0, 300)
+            pay_now = input(f"Your total amount is ${total_amount}. Would you like to pay this now? Y/N ")
+            if pay_now.lower() in ("y", "yes"):
+                self.current_ticket[pay] = True
+                print("Your payment is confirmed. You have 15 minutes to exit the premises. Or else.")
+            elif pay_now.lower() in ('n', 'no'):
+                print("Good luck, you're stuck forever, mwahahaha!")
+            else:
+                print("Please pick a valid option.")          
         
-my_garage = ParkingGarage(100,100)       
+my_garage = GarageTransactions()       
 
 def run():
     while True:
@@ -64,12 +76,13 @@ def run():
             Welcome to our Parking Garage! We have "state of the art" parking lots here and we're not afraid to overcharge!
             Are you here to: 
             1. Purchase a parking ticket
-            2. Inquire about available parking lots
-            3. Pay for your ticket
-            4. Leave the Parking Garage
-            5. Quit/Exit
+            2. Inquire about available parking spaces
+            3. Check if your ticket is paid
+            4. Pay for your ticket
+            5. Leave the Parking Garage
+            6. Quit/Exit
             """)
-        if user_input == '5':
+        if user_input == '6':
             print("I hope you paid for your parking while you were here, other wise we'll hunt you down")
             break
         elif user_input == '1':
@@ -77,8 +90,10 @@ def run():
         elif user_input == '2':
             my_garage.show_available_spaces()
         elif user_input == '3':
-            my_garage.pay_for_parking()
+            my_garage.check_if_paid()
         elif user_input == '4':
+            my_garage.pay_for_parking()
+        elif user_input == '5':
             my_garage.leave_garage()
         else:
             print("Please enter a valid number item 1-5 for the list of choices available")
